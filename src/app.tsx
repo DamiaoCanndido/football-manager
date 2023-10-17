c0376914bd80aee468cea59735b4f96073497bce
DamiaoCanndido <tower_gamer@hotmail.com>
2023-10-17 19:32:39 -0300

    create main page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
+import { Search, Filter } from 'lucide-react';
+import { Button } from './components/ui/button';
+
 export function App() {
-  return <h1>Football Manager</h1>;
+  return (
+    <div className="min-h-screen bg-white flex flex-col">
+      <nav className="bg-neutral-500 flex px-6 py-3 items-center justify-between border-b">
+        Football Manager
+        <div className="flex items-center">
+          <form className="flex items-center" onSubmit={() => {}}>
+            <Button className="bg-transparent hover:bg-transparent">
+              <Search />
+            </Button>
+            <input className="p-1" type="text" />
+          </form>
+          <Button className="bg-transparent hover:bg-transparent">
+            <Filter />
+          </Button>
+        </div>
+      </nav>
+      <div className="flex flex-1">
+        <aside className="w-40 flex bg-gray-500"></aside>
+        <main></main>
+      </div>
+    </div>
+  );
 }
