a8d74cba24c95b4bf6f0bf3dcc1d7bbf7103acd2
DamiaoCanndido <tower_gamer@hotmail.com>
2023-10-17 21:49:36 -0300

    create side bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,18 @@
-import { Search, Filter } from 'lucide-react';
+import { Search, Filter, Trophy, UserCheck, Swords } from 'lucide-react';
+import { Input } from './components/ui/input';
 import { Button } from './components/ui/button';
 
 export function App() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <nav className="bg-neutral-500 flex px-6 py-3 items-center justify-between border-b">
-        Football Manager
+      <nav className="bg-gray-500 flex px-6 py-3 items-center justify-between border-b">
+        <h2 className="text-white">Football Manager</h2>
         <div className="flex items-center">
           <form className="flex items-center" onSubmit={() => {}}>
             <Button className="bg-transparent hover:bg-transparent">
               <Search />
             </Button>
-            <input className="p-1" type="text" />
+            <Input className="px-1 mx-1" />
           </form>
           <Button className="bg-transparent hover:bg-transparent">
             <Filter />
@@ -19,7 +20,20 @@ export function App() {
         </div>
       </nav>
       <div className="flex flex-1">
-        <aside className="w-40 flex bg-gray-500"></aside>
+        <aside className="w-60 p-2 flex flex-col bg-gray-500">
+          <Button className="mb-2 bg-transparent hover:bg-gray-400">
+            <Swords className="mx-3" />
+            Jogos
+          </Button>
+          <Button className="mb-2 bg-transparent hover:bg-gray-400">
+            <Trophy className="mx-3" />
+            Ligas
+          </Button>
+          <Button className="bg-transparent hover:bg-gray-400">
+            <UserCheck className="mx-3" />
+            Times
+          </Button>
+        </aside>
         <main></main>
       </div>
     </div>
